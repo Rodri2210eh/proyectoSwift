e30bf18f10962cb32750f69f38e6d2efdac3d90c
Rodri2210eh <naliuska.espher@gmail.com>
2021-06-22 13:31:34 -0600

    Agregando el menu

--- cliente.swift
+++ cliente.swift
@@ -17,24 +17,31 @@ class cliente{
     }
 
     func mostrar_medidores() -> Void {
+        var cont = 0
         for medi in medidores {
-            print("Id: \(medi.id)")
-            print("Plan: \(medi.plan.nombre)")
-            print("Volteos: \(medi.cantV)")
+            print("\(cont).  Id: \(medi.id)  Plan: \(medi.plan.nombre)  Volteos: \(medi.cantV)")
+            cont += 1
         }
     }
 
-    func mostrar_cobros(ide:String) -> Void {
+    func mostrar_cobros(indice:Int) -> Void {
+        var ind = 0
         for me in medidores{
-            if(me.id == ide){
+            if(ind == indice){
+                print("Id: \(me.id)  Plan: \(me.plan.nombre)")
+                var cont = 0
                 for cob in me.cobros {
-                print("Id: \(cob.identificador)")
-                print("Estado: \(cob.estado)")
-                print("Fecha: \(cob.fecha)")
-                print("Monto: \(cob.monto)")
+                    print("\(cont).  Id: \(cob.identificador)  Estado: \(cob.estado)  Fecha: \(cob.fecha)  Monto: \(cob.monto)")
+                    cont += 1
                 }
             }
+            ind += 1
         }
+    }
+
+    func agregarMedidor(id:String, plan:Plan) -> Void{
+        let m = medidor(id: id, plan: plan)
+        medidores.append(m)
     }
 
 
@@ -53,6 +60,12 @@ class medidor{
         self.plan = plan
         self.cantV = 0
         self.cobros = Array<cobro>()
+    }
+
+
+    func agregarCobros(ident:Int, estado:String, fecha:Date, monto:Int){
+        let c = cobro(identificador: ident,estado: estado,fecha: fecha,monto: monto)
+        cobros.append(c)
     }
 
 }
@@ -120,5 +133,103 @@ class apelacion{
     }
 }
 
+var clientes:Array<cliente> = Array<cliente>()
 
+var agentes:Array<agente> = Array<agente>()
+
+
+var t = 0
+while(t == 0){
+
+    print("Bienvenido")
+    print("Ingrese 1 para iniciar como un cliente")
+    print("Ingrese 2 para iniciar como un Agente")
+    let eleccion1 = readLine()
+
+
+
+    print("Ingrese su usuario... ")
+    let usuar = readLine()
+    print("Ingrese su contraseña... ")
+    let passw = readLine()
+
+    if (eleccion1 == "1"){
+        for client in clientes{
+            if(client.username == usuar){
+                if(client.password == passw){
+                    print("Bienvendo \(client.nombre)")
+                    while(t == 0){
+                        print("Elija una opcion para continuar")
+                        print("1.   Ver medidores")
+                        print("2.   Ver informacion de la cuenta")
+                        print("3.   Realizar apelacion")
+                        print("4.   Salir")
+                        var r2 = readLine()
+                        if(r2 == "1"){
+                            print("Elija el medidor que desea ver")
+                            client.mostrar_medidores()
+                            print("z.   Salir")
+                            r2 = readLine()
+                            if(r2 != "z"){
+                                let r3 = Int(r2!)
+                                print("Los cobros del medidor son:")
+                                client.mostrar_cobros(indice: r3!)
+                                r2 = readLine()
+                            }
+                        }
+                        else if(r2 == "2"){
+                            print("Nombre: \(client.nombre)")
+                            print("Correo Electronico: \(client.correo)")
+                            print("Usuario: \(client.username)")
+                            print("Password: *********")
+                        }
+                        else if(r2 == "3"){
+                            print("Ingrese su usuario")
+                            let ap1 = readLine()
+                            print("Ingrese el Id del medidor")
+                            let ap2 = readLine()
+                            print("Ingrese el motivo de su apelacion")
+                            let ap3 = readLine()
+                            print("Su apelacion ha sido enviada, Usuario: \(ap1!), medidor: \(ap2!), motivo: \(ap3!)")
+                        }
+                        else if(r2 == "4"){
+                            t = 4
+                        }
+                        else{
+                            print("Entrada invalida, vuelva a intentarlo")
+                        }
+                    }
+                }
+                else{
+                    print("Contraseña invalida, vuelva a intentarlo")
+                }
+            }
+            else{
+                print("Usuario invalido, vuelva a intentarlo")
+            }
+        }
+
+    }
+    else if(eleccion1 == "2"){
+        for agent in agentes{
+            if(agent.user == usuar){
+                if(agent.password == passw){
+                    t = 1
+                }
+                else{
+                    print("Contraseña invalida, vuelva a intentarlo")
+                }
+            }
+            else{
+                print("Usuario invalido, vuelva a intentarlo")
+            }
+        }
+    }
+    else{
+        print("Entrada invalida, vuelva a intentarlo")
+    }
+    
+    print("Error en los datos vuelva a intentarlo")
+
+}
 
